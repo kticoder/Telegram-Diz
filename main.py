2a960e8f3f7f2466408ea94c08881d3d121c7e9d
Kadir Tarık İlgin <84983622+kadirilgin1453@users.noreply.github.com>
2022-03-15 21:09:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from telethon.sync import TelegramClient, events
+import os
+from Ayarlar import *
+from rich import *
+from rich.console import *
+konsol = Console()
+
+konsol.print("""[bold]---------------------------------- Program Başlatıldı | İndirme Başladı---------------------------------------------------------
+t.me/kadirilgin1453 Herhangi Bir Sorunuz Yada Sorununuz Varsa Telegramdan Ulaşabilirsiniz.[/]""")
+
+api_id = TG_APIID
+api_hash = TG_APIHash
+
+dosyakontrolFonksiyonu(api_hash,api_hash)
+
+with TelegramClient('session.session', TG_APIID, TG_APIHash) as client:
+    messages = client.iter_messages(TG_KanalAdi)
+    os.mkdir(TG_KanalAdi)
+    os.chdir(TG_KanalAdi) 
+    for msg in messages:
+        icerik = msg.media
+        if Resim == "Evet":
+            if "MessageMediaPhoto" in str(icerik):
+                resimindir(msg)
+        if Video == "Evet":
+            if "video/mp4" in str(icerik):
+                videoindir(msg)
+        if Pdf == "Evet":
+            if "application/pdf" in str(icerik):
+                pdfindir(msg)
+        if DigerDosyalar == "Evet":
+            if "application/rar" in str(icerik):
+                dosyaindir(msg)
+            if "application/zip" in str(icerik):
+                dosyaindir(msg)
+            if "pplication/vnd.android.package-archive" in str(icerik):
+                dosyaindir(msg)
